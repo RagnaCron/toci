671b62ca5a289a65fca56494d883cac45e28cbaa
Manuel Werder <manuelwerder@hotmail.com>
2025-10-21 15:18:00 +0200

    Changed main sig for cli commands. Fixed an include from the cmake (assets) no longer available.

--- src/main.c
+++ src/main.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 
-int main(void) {
+int main(int argc, int* argv[]) {
+    printf("%d\n", argc);
+    printf("%s\n", *argv);
+    printf("%s\n", argv[1]);
     printf("Hello, World!\n");
     return 0;
 }
