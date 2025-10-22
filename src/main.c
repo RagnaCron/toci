904f3106d9651630702474e1fedd0bd8a92cfbaf
Manuel Werder <manuelwerder@hotmail.com>
2025-10-22 22:12:16 +0200

    Running in some bugs in fix command.. )(

--- src/main.c
+++ src/main.c
@@ -1,5 +1,6 @@
 #include <errno.h>
 #include <stdbool.h>
+#include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -14,6 +15,7 @@
 void printUsage(void);
 int listTodos(const char *file_name);
 int newTodos(const char *file_name);
+int fixTodos(const char *file_name);
 
 int main(const int argc, const char *argv[]) {
     if (argc < 2) {
@@ -34,6 +36,9 @@ int main(const int argc, const char *argv[]) {
         return newTodos(argv[2]);
     }
 
+    if (strcmp(argv[1], "fix") == 0 && argc == 3) {
+        return fixTodos(argv[2]);
+    }
     return EXIT_SUCCESS;
 }
 
@@ -44,9 +49,10 @@ void printUsage(void) {
            "\tlist <filename>\tThis will list all todos.\n"
            "\tnew <filename>\tThis will create a new todo file or\n"
            "\t\t\tappend to an existing one.\n"
+           "\tfix <filename>\tThis will add a checkbox if one is missing.\n"
            "\nInfo:\n"
            "\tThe todo should be on a single line.\n"
-           "\tThe line length should not be more then 254 characters\n"
+           "\tThe line length should not be more then 121 characters\n"
            "\tor it will be cut of...\n"
            "\n");
 }
@@ -92,12 +98,21 @@ static int read_line(char line[]) {
     }
     line[i] = '\0';
 
+    if (c == EOF) {
+        flush_stdin(); // to leave a clean stdin state
+    }
+
     return c == EOF ? EOF : i;
 }
 
-void ensure_checkbox_prefix(char line[]) {
-    if (strncmp(line, CHECKBOX_UNCHECKED, strlen(CHECKBOX_UNCHECKED)) == 0 ||
-        strncmp(line, CHECKBOX_CHECKED, strlen(CHECKBOX_CHECKED)) == 0) {
+static bool hasCheckbox(char line[]) {
+    return (strncmp(line, CHECKBOX_UNCHECKED, strlen(CHECKBOX_UNCHECKED)) ==
+                0 ||
+            strncmp(line, CHECKBOX_CHECKED, strlen(CHECKBOX_CHECKED)) == 0);
+}
+
+static void ensureCheckboxPrefix(char line[]) {
+    if (hasCheckbox(line)) {
         return;
     }
 
@@ -131,11 +146,78 @@ int newTodos(const char *file_name) {
             continue;
         }
 
-        ensure_checkbox_prefix(line);
+        ensureCheckboxPrefix(line);
 
         fprintf(fptr, "%s", line);
     }
 
     fclose(fptr);
+    return EXIT_SUCCESS;
+}
+
+static void trimNewLine(char line[]) { line[strcspn(line, "\n")] = '\0'; }
+
+static int handleLongLine(char line[]) {
+    int state = 0;
+
+    for (;;) {
+        printf("\nLine to long (%zu chars):\n", strlen(line));
+        printf("%s", line);
+        printf("\n\n");
+        printf("Enter <ctrl + D> to remove this line.\n");
+        printf("Enter a shorter version (max %d chars):\n", LINE);
+
+        fflush(stdout);
+
+        char new_line[MAX_LINE];
+        state = read_line(new_line);
+        strcpy(new_line, line);
+
+        if (state == EOF) {
+            clearerr(stdin);
+            break;
+        }
+
+        if (state == LINE) {
+            break;
+        }
+    }
+
+    return state;
+}
+
+int fixTodos(const char *file_name) {
+    FILE *in = fopen(file_name, "r");
+    FILE *out = fopen("todo.tmp", "w");
+    if (in == NULL || out == NULL) {
+        perror("Error opening file");
+        fprintf(stderr, "Error code: %d\n", errno);
+        return errno;
+    }
+
+    char line[MAX_LINE];
+    while (fgets(line, MAX_LINE, in)) {
+        trimNewLine(line);
+
+        if (strlen(line) > LINE) {
+            int state = handleLongLine(line);
+            if (state == EOF) {
+                continue;
+            }
+        }
+
+        if (!hasCheckbox(line)) {
+            ensureCheckboxPrefix(line);
+        }
+
+        fprintf(out, "%s\n", line);
+    }
+
+    fclose(in);
+    fclose(out);
+
+    remove(file_name);
+    rename("todo.tmp", file_name);
+
     return EXIT_SUCCESS;
 }
